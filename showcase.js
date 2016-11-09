adc228a95c987c1d4eec3dc1cce3898a69310292
Vito Fan <vitogalaxy@hotmail.com>
2016-11-09 08:18:53 +0800

    UPDATE TO RWD

--- showcase.js
+++ showcase.js
@@ -1,29 +1,62 @@
 document.addEventListener('DOMContentLoaded', function () {
-	var rects = document.getElementsByClassName('skill-rect');
-	var expertise = document.getElementsByClassName('expertise');
+	var frontendRects = document.getElementsByClassName('frontendRects');
+	var frontendExpertise = document.getElementsByClassName('frontendExpertise');
+	var projectsMenuLabel = document.getElementsByClassName('projectsMenuLabel');
+	var mobileLabel = document.getElementsByClassName('mobileLabel');
+	var carouselStart = document.getElementsByClassName('carousel-start');
 
 	function indexOfRect(ele) {
-		for (var i = 0; i < rects.length; i++) {
-			if(rects[i] == ele) {
+		for (var i = 0; i < frontendRects.length; i++) {
+			if(frontendRects[i] == ele) {
 				return i;
 			}
 		}
 			return -1;
 	}
 
+	function indexOfEle(ele, array) {
+		for (var i = 0; i < array.length; i++) {
+			if (array[i] == ele) {
+				return i;
+			}
+		}
+
+		return -1;
+
+	}
+
 	function rectHandlerEnter() {
 		var k = indexOfRect(this);
-		expertise[k].style.opacity = 1;
+		frontendExpertise[k].style.opacity = 1;
 	}
 
 	function rectHandlerLeave() {
 		var j = indexOfRect(this);
-		expertise[j].style.opacity = 0;
+		frontendExpertise[j].style.opacity = 0;
 	}
 
-	for (var i = 0; i < rects.length; i++) {
-		rects[i].addEventListener('mouseenter', rectHandlerEnter);
-		rects[i].addEventListener('mouseleave', rectHandlerLeave);
+	function toggleDisplay(array, k) {
+		for (var i = 0; i < array.length; i++) {
+			array[i].removeAttribute("checked")
+			if (i === k) {
+				array[i].setAttribute("checked", "")
+			}
+		}
+	}
+
+	function projectsMenuLabelHandler() {
+		var q = indexOfEle(this, projectsMenuLabel);
+		toggleDisplay(carouselStart, q)
+		var element = mobileLabel[q];
+		element.scrollIntoView();
+	}
+
+	for (var i = 0; i < frontendRects.length; i++) {
+		frontendRects[i].addEventListener('mouseenter', rectHandlerEnter);
+		frontendRects[i].addEventListener('mouseleave', rectHandlerLeave);
+	}
+	for (var i = 0; i < projectsMenuLabel.length; i++) {
+		projectsMenuLabel[i].addEventListener('click', projectsMenuLabelHandler)
 	}
 
 });
